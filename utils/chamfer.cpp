1e6b62c195acbe442be4e2141b7a7a3e89d794d0
Vineet Kosaraju <vineetsk1@gmail.com>
2019-07-26 23:20:33 -0700

    re-enable gpu

--- utils/chamfer.cpp
+++ utils/chamfer.cpp
@@ -4,10 +4,10 @@ void nnsearch(const int b, const int n, const int m,
               const float* xyz1, const float* xyz2,
               float* dist, int* idx);
 
-// int CDKernelLauncher(const int b, const int n,
-//                      const float* xyz1, const int m,
-//                      const float* xyz2, float* dist1,
-//                      int* idx1, float* dist2, int* idx2);
+int CDKernelLauncher(const int b, const int n,
+                     const float* xyz1, const int m,
+                     const float* xyz2, float* dist1,
+                     int* idx1, float* dist2, int* idx2);
 
 // Chamfer Distance
 // Forward implementation
@@ -68,18 +68,18 @@ void nnsearch(const int b, const int n, const int m,
 // Chamfer Distance
 // Forward implementation
 // CUDA
-// void cd_forward_cuda(const at::Tensor xyz1,
-//                      const at::Tensor xyz2,
-//                      const at::Tensor dist1,
-//                      const at::Tensor dist2,
-//                      const at::Tensor idx1,
-//                      const at::Tensor idx2) {
-//     CDKernelLauncher(xyz1.size(0), xyz1.size(1), xyz1.data<float>(),
-//                      xyz2.size(1), xyz2.data<float>(), dist2.data<float>(),
-//                      idx1.data<int>(), dist2.data<float>(), idx2.data<int>());
-// }
+void cd_forward_cuda(const at::Tensor xyz1,
+                     const at::Tensor xyz2,
+                     const at::Tensor dist1,
+                     const at::Tensor dist2,
+                     const at::Tensor idx1,
+                     const at::Tensor idx2) {
+    CDKernelLauncher(xyz1.size(0), xyz1.size(1), xyz1.data<float>(),
+                     xyz2.size(1), xyz2.data<float>(), dist2.data<float>(),
+                     idx1.data<int>(), dist2.data<float>(), idx2.data<int>());
+}
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
     m.def("forward", &cd_forward, "CD forward");
-    // m.def("forward_cuda", &cd_forward_cuda, "CD forward CUDA");
+    m.def("forward_cuda", &cd_forward_cuda, "CD forward CUDA");
 }
