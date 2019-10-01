1c12598c469262848fcb3b858272feb18a470b52
Vineet Kosaraju <vineetsk1@gmail.com>
2019-10-01 09:39:16 -0700

    minor update

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from datasets.shapenet import Shapenet
+
 from tasks.master import Master
 from tasks.sampler_task import SamplerTask
 from tasks.chamfer_task import ChamferTask
@@ -5,13 +7,6 @@ from tasks.icp_task import ICPTask
 from tasks.icp_task import CandidateICPTask
 
 import queue
-
-class FakeDataset:
-
-    def load(self, mesh_id):
-        import pywavefront
-        obj = pywavefront.Wavefront("static/obj/teapot.obj", create_materials=True, collect_faces=True)
-        return obj
 
 # cache = "cache"
 # dataset = FakeDataset()
@@ -26,7 +21,7 @@ class FakeDataset:
 
 # ICP between mesh and candidate meshes
 cache = "cache"
-dataset = FakeDataset()
+dataset = Shapenet(path="/cvgl/group/ShapeNetCore_v2/ShapeNetCore_v2/")
 inputs = (dataset, "teapot", [dataset, "teapot", dataset, "teapot", dataset, "teapot"])
 core_task = CandidateICPTask(cache, inputs)
 
